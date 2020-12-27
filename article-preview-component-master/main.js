5b9d7a23c2f1cbf9149524b53e8fdbbcf90a213f
NelsonBaileyIII <nelsonbailey@Nelsons-MBP.hsd1.ga.comcast.net>
2020-12-26 22:03:00 -0500

    4th practice project

---
+++ article-preview-component-master/main.js
@@ -0,0 +1,9 @@
+// let $share = document.getElementsByID("share");
+let $overcontain = document.getElementById("overContain")
+
+function show(){
+    $overcontain.style.visibility = "visible";
+}
+function hide(){
+    $overcontain.style.visibility = "hidden";
+}
